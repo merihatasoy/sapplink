8faba0f343075282fe3d4a535e907923c6605972
merih atasoy <merih.atasoy@mail.utoronto.ca>
2019-11-27 19:26:35 -0500

    refactor

--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndexPageComponent } from './index-page/index-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexPageComponent
   ],
   imports: [
     BrowserModule
