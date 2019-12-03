98c1800ea6fdd1e255a9f9d592e4d9e23dc8554b
merih atasoy <merih.atasoy@mail.utoronto.ca>
2019-12-02 19:40:15 -0500

    commit

--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { IndexPageComponent } from './index-page/index-page.component';
+import { SkillItemComponent } from './skill-item/skill-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexPageComponent
+    IndexPageComponent,
+    SkillItemComponent
   ],
   imports: [
     BrowserModule
